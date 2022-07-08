1e3cc61c89368ff4b9bcd20bb68390b6404a3cad
SparrowMike <michal.m_wroblewski@hotmail.com>
2022-07-08 23:46:54 +0800

    feat: 🎸 major features implemented before final modifications

--- babySlack/public/jointNs.js
+++ babySlack/public/jointNs.js
@@ -1,5 +1,5 @@
 function joinNs(endpoint) {
-    const nsSocket = io(`http://localhost:9000${endpoint}`)
+    nsSocket = io(`http://localhost:9000${endpoint}`)
     nsSocket.on('nsRoomLoad', (nsRooms) => {
         console.log(nsRooms)
         let roomList = document.querySelector('.room-list');
@@ -21,18 +21,40 @@ function joinNs(endpoint) {
                 console.log('Clicked', e.target.innerText)
             });
         });
+        const topRoom = document.querySelector('.room');
+        const topRoomName = topRoom.innerText
+        joinRoom(topRoomName)
     });
 
     nsSocket.on('messageToClients', (msg) => {
+        const newMsg = buildHTML(msg)
         console.log(msg)
-        document.querySelector('#messages').innerHTML += `<li>${msg.text}</li>`;
+        document.querySelector('#messages').innerHTML += newMsg;
     });
 
     document.querySelector('.message-form').addEventListener('submit', (event) => {
         event.preventDefault();
+
         const newMessage = document.querySelector('#user-message').value;
+        console.log(newMessage)
         if (newMessage == '') return;
-        socket.emit('newMessageToServere', { text: newMessage });
+        nsSocket.emit('newMessageToServer', { text: newMessage });
         document.querySelector('#user-message').value = '';
     });
+}
+
+function buildHTML(msg) {
+    const convertedDate = new Date(msg.time).toLocaleString();
+    const newHTML = `
+    <li>
+        <div class="user-image">
+            <img src="${msg.avatar}" />
+        </div>
+        <div class="user-message">
+            <div class="user-name-time">${msg.username} <span>${convertedDate}</span></div>
+            <div class="message-text">${msg.text}</div>
+        </div>
+    </li>
+    `
+    return newHTML;
 }
--- babySlack/slack.js
+++ babySlack/slack.js
@@ -25,5 +25,31 @@ namespaces.forEach((namespace) => {
         console.log(`${nsSocket.id} has join ${namespace.endpoint}`)
 
         nsSocket.emit('nsRoomLoad', namespaces[0].rooms)
+        nsSocket.on('joinRoom', async (roomToJoin, numberOfUsersCallback) => {
+            nsSocket.join(roomToJoin)
+            const clients = await io.of('/wiki').in(roomToJoin).allSockets();
+            // numberOfUsersCallback(Array.from(clients).length)
+            
+            const nsRoom = namespaces[0].rooms.find(room => {
+                return room.roomTitle === roomToJoin;
+            });
+
+            nsSocket.emit('historyCatchUp', nsRoom.history)
+            io.of('wiki').in(roomToJoin).emit('updateMembers', Array.from(clients).length)
+        });
+        nsSocket.on('newMessageToServer', (msg) => {
+            const fullMsg = {
+                text: msg.text,
+                time: Date.now(),
+                username: 'dude',
+                avatar: 'https://via.placeholder.com/30'
+            }
+            const roomTitle = Array.from(nsSocket.rooms)[1];
+            const nsRoom = namespaces[0].rooms.find(room => {
+                return room.roomTitle === roomTitle;
+            });
+            nsRoom.addMessage(fullMsg)
+            io.of('/wiki').to(roomTitle).emit('messageToClients', fullMsg);
+        });
     });
 });

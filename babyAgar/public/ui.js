7694b177091b4f55caeb49891f57de361771f44c
SparrowMike <michal.m_wroblewski@hotmail.com>
2022-08-01 23:50:09 +0800

    style: 💄 UI startup

---
+++ babyAgar/public/ui.js
@@ -0,0 +1,26 @@
+let wHeight = $(window).height();
+let wWidth = $(window).width();
+let player = {};
+
+let canvas = document.querySelector('#the-canvas');
+let context = canvas.getContext('2d');
+canvas.width = wWidth;
+canvas.height = wHeight;
+
+$(window).load(() => {
+    $('#loginModal').modal('show');
+});
+
+$('.name-form').submit((event) => {
+    event.preventDefault();
+    player.name = $('#name-input').val()
+    $('#loginModal').modal('hide');
+    $('#spawnModal').modal('show');
+    $('.player-name').html(player.name);
+});
+
+$('.start-game').click(() => {
+    $('.modal').modal('hide');
+    $('.hiddenOnStart').removeAttr('hidden');
+    init();
+});

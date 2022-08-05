617204519cd2c8d0c3a6e9e770669ea7e82f72a8
SparrowMike <michal.m_wroblewski@hotmail.com>
2022-08-05 23:58:17 +0800

    fix: 🐛 typo

--- babyAgar/sockets/classes/Orb.js
+++ babyAgar/sockets/classes/Orb.js
@@ -1,8 +1,8 @@
 class Orb {
     constructor(settings) {
         this.color = this.getRandomColor();
-        this.locX = Math.floor(Math.random() * settings.Width);
-        this.locY = Math.floor(Math.random() * settings.Height);
+        this.locX = Math.floor(Math.random() * settings.worldWidth);
+        this.locY = Math.floor(Math.random() * settings.worldHeight);
         this.radius = 5;
     }
 
